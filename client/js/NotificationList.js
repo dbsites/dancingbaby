92c3d4d89f3b61d01efa32c21703488ffdb4f82d
neilkatz <nkatz@gaptronic.com>
2019-05-22 16:18:26 -0700

    js folder

---
+++ client/js/NotificationList.js
@@ -0,0 +1,13 @@
+export const NotificationList = {
+
+    INIT_DATA_LOADED: 'INIT_DATA_LOADED',
+
+    // ########################### USER EVENTS ###########################
+    USER_LOGGED_IN: 'USER_LOGGED_IN',
+    USER_LOGGED_OUT: 'USER_LOGGED_OUT',
+    USER_NOT_LOGGED_IN: 'USER_NOT_LOGGED_IN',
+
+    // ########################### LOADING MODAL EVENTS ###########################
+    SHOW_LOADING: 'SHOW_LOADING',
+    HIDE_LOADING: 'HIDE_LOADING'
+};
