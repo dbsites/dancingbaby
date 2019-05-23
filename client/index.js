17cea34f7a71187b19a28d100c21428b7deaabb4
neilkatz <nkatz@gaptronic.com>
2019-05-22 21:57:05 -0700

    inporting index.scss rather then styles

--- client/index.js
+++ client/index.js
@@ -14,7 +14,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
 import store from './store';
-import './styles.css';
+import './styles/index.scss';
 
 render(
     <Provider store={store}>
