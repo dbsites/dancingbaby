9cc742556e2858175cb0fa4c425dca8211722f94
neilkatz <nkatz@gaptronic.com>
2019-05-26 17:36:52 -0700

    added redux form reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -10,6 +10,7 @@
  */
 
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form'
 
 // import all reducers here
 import cardsReducer from './cardsReducer';
@@ -19,6 +20,7 @@ import cardsReducer from './cardsReducer';
 // combine reducers
 const reducers = combineReducers({
   cards: cardsReducer,
+    form: formReducer
 });
 
 // make the combined reducers available for import
