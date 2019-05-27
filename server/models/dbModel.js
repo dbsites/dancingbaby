5a46c525b40c8d1132431b24b042cc83574fbbe0
smozingo <schno.mozingo@gmail.com>
2019-05-26 17:27:50 -0700

    modularized logging and sessions

--- server/models/dbModel.js
+++ server/models/dbModel.js
@@ -1,4 +1,6 @@
 const pg = require('pg');
+const { logger } = require('../util/loggingUtil');
+
 
 // create a config to configure both pooling behavior and client options
 // note: values here should be derived from environment variables 
@@ -23,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
   config.port = 5432;
 }
 
-console.log(`Connecting to database ${config.database} on host ${config.host}`);
+logger.info(`Connecting to database ${config.database} on host ${config.host}`);
 
 //this initializes a connection pool
 //it will keep idle connections open for 30 seconds
@@ -37,17 +39,18 @@ const pool = new pg.Pool(config);
 // between your application and the database, the database restarts, etc.
 // and so you might want to handle it and at least log it out
 pool.on('error', function (err, client) {
-  console.error('idle client error', err.message, err.stack);
+  logger.error('idle client error', err.message, err.stack);
 });
 
 //export the query method for passing queries to the pool
-module.exports.query = function (text, values, callback) {
-  console.log('query:', text, values);
+const query = function (text, values, callback) {
   return pool.query(text, values, callback);
 };
 
 // the pool also supports checking out a client for
 // multiple operations, such as a transaction
-module.exports.connect = function (callback) {
+const connect = function (callback) {
   return pool.connect(callback);
 };
+
+module.exports = { pool, query, connect }
--- server/server.js
+++ server/server.js
@@ -3,42 +3,25 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('config');
-const logger = require('morgan');
 const session = require('express-session');
 const flash = require('req-flash');
-const moment = require('moment-timezone');
-const pgSession = require('connect-pg-simple')(session);
-const passport = require('./config/passport');
-const pgPool = require('./models/dbModel');
+const passport = require('./controllers/passport');
+const { pool, connect } = require('./models/dbModel');
 const marketController = require('./controllers/marketController');
 const cardController = require('./controllers/cardController');
 const accountController = require('./controllers/accountController');
-
+const { logger, morgan } = require('./util/loggingUtil');
+const { sessionConfig, secret } = require('./util/sessionConfig');
 
 const app = express();
 
 const PORT = process.env.PORT || '3000';
 
-app.use(logger(':date[clf] :method :url :status :response-time ms - :res[content-length]'));
+app.use(morgan('localtz'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../dist/')));
-app.use((req, res, next) => { console.log(req.body); next(); });
-
-
-// session logic
-const secret = process.env.SESSION_SECRET || config.get('session-secret');
-const sessionConfig = {
-  secret,
-  name: 'dancingbaby',
-  resave: false, // only resave session if changes are made to session
-  saveUninitialized: false, // only create sessions for users who log in
-  store: new pgSession({ pool: pgPool }), // store user session data in database
-  cookie: { secure: true, maxAge: 2 * 24 * 60 * 60 * 1000 } // 2 day session
-}
-if (process.env.NODE_ENV !== 'production') {
-  sessionConfig.cookie.secure = false; // set cookies on http for dev
-}
+app.use((req, res, next) => { logger.info(req.body); next(); });
 
 app.use(session(sessionConfig));
 app.use(passport.initialize());
@@ -71,7 +54,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-    console.error(`${moment()}> ${err.status ? err.status : ''} ${err.message}`);
+    logger.error(`${err.status ? err.status : ''} ${err.message}`);
     if (err.flash) {
         res.status(err.status || 500).json(err.flash);
     } else {
@@ -80,7 +63,8 @@ app.use(function(err, req, res, next) {
 });
 
 app.listen(PORT, (err) => {
-  console.log(`${moment().tz("America/Los_Angeles").format('YYYY-MM-DD HH:mm:ss')} >`, err || 'server listening on port '  + PORT);
+  //console.log(`${moment().tz("America/Los_Angeles").format('YYYY-MM-DD HH:mm:ss')} >`, err || 'server listening on port '  + PORT);
+  logger.info(err || 'server listening on port '  + PORT);
 });
 
 module.exports = app;
