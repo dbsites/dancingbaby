291ccdc474e1129b69ac9bd5d4b6c3db2f9b4640
neilkatz <nkatz@gaptronic.com>
2019-06-02 18:25:17 -0700

    added user action types

--- client/constants/actionTypes.js
+++ client/constants/actionTypes.js
@@ -2,8 +2,8 @@
  * ************************************
  *
  * @module  actionTypes.js
- * @author  smozingo
- * @date    11/13/17
+ * @author  katzman
+ * @date    06.02.2019
  * @description Action Type Constants
  *
  * ************************************
@@ -14,3 +14,11 @@ export const ADD_CARD = "ADD_CARD";
 export const DELETE_CARD = "DELETE_CARD";
 export const SET_NEW_LOCATION = "SET_NEW_LOCATION";
 export const UPDATE_MARKETS = "UPDATE_MARKETS";
+
+
+export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
+export const USER_LOGIN_UNAUTHORIZED = "USER_LOGIN_UNAUTHORIZED";
+export const USER_LOGIN_ERROR = "USER_LOGIN_ERROR";
+
+export const UPDATE_ASSESSMENT = "UPDATE_ASSESSMENT";
+
