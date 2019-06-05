2e72bbbc2d2e96c482aca90060c5fd2affd4afba
neilkatz <nkatz@gaptronic.com>
2019-06-04 23:10:12 -0700

    discovered CSS grid.

--- client/components/DisclaimerComponent.jsx
+++ client/components/DisclaimerComponent.jsx
@@ -15,7 +15,7 @@ import dbLogo from '../assets/svg/db_logo_greenyellow.svg';
 import checkmark from '../assets/svg/checkMark.svg';
 
 
-let DisclaimerComponent = ( props ) =>
+const DisclaimerComponent = ( props ) =>
 {
     const { openContact, handleSubmit, onSubmit } = props;
 
@@ -31,33 +31,39 @@ let DisclaimerComponent = ( props ) =>
             </div>
 
             <div className='disclaimerBox'>
-                <div className='checkBoxContainer'>
-                    <div className='checkbox'>
-                        <div className='checkMark'>
-                            <img src={checkmark} className='checkmark' alt='checkmark' />
-                        </div>
-                    </div>
+                <div className='checkBoxContainer top'>
+                    <CheckBox checked={true} />
                     <div>I understand that this assessment tool is for demonstration purposes only and does not constitute legal advice.</div>
                 </div>
-                <div className='checkBoxContainer'>
-                    <div className='checkbox'>
-                        <div className='checkMark'>
-                            <img src={checkmark} className='checkmark' alt='checkmark' />
-                        </div>
-                    </div>
+                <div className='checkBoxContainer middle'>
+                    <CheckBox/>
                     <div>I understand that the information contained in this demonstration is confidential, copyrighted, may constitute trade secrets, and may not be shared with others without permission</div>
                 </div>
-                <div className='checkBoxContainer'>
-                    <div className='checkbox'>
-                        <div className='checkMark'>
-                            <img src={checkmark} className='checkmark' alt='checkmark' />
-                        </div>
-                    </div>
+                <div className='checkBoxContainer bottom'>
+                    <CheckBox checked={false} />
                     <div>I have read and agree to the Terms of Use and Privacy Policy.</div>
                 </div>
             </div>
         </div>
     );
+};
+
+const CheckBox = ( props ) =>
+{
+    const updateCheck = () =>
+    {
+        console.log( "UPDATE CHECK CLICKED" );
+    };
+
+    const checkMarkCls = props.checked ? 'checkMark' : 'checkMark hide';
+
+    return (
+        <div onClick={updateCheck} className='checkbox'>
+            <div className={checkMarkCls}>
+                <img src={checkmark} className='checkmark' alt='checkmark' />
+            </div>
+        </div>
+    )
 };
 
 
