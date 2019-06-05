093faeb5208d096a2dff6d500417044ea0d09bfe
neilkatz <nkatz@gaptronic.com>
2019-06-04 17:00:04 -0700

    added types for questions

--- client/constants/actionTypes.js
+++ client/constants/actionTypes.js
@@ -15,10 +15,17 @@ export const DELETE_CARD = "DELETE_CARD";
 export const SET_NEW_LOCATION = "SET_NEW_LOCATION";
 export const UPDATE_MARKETS = "UPDATE_MARKETS";
 
-
+// user specific.
 export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
 export const USER_LOGIN_UNAUTHORIZED = "USER_LOGIN_UNAUTHORIZED";
 export const USER_LOGIN_ERROR = "USER_LOGIN_ERROR";
 
+
+// current page the user is on.
+export const UPDATE_CURRENT_PAGE = "UPDATE_CURRENT_PAGE";
+
+
+// questions/assessment specific.
 export const UPDATE_ASSESSMENT = "UPDATE_ASSESSMENT";
+export const QUESTION_ANSWERED = "QUESTION_ANSWERED";
 
