cf617b417548f2935d960ec6686451c576d694e0
neilkatz <nkatz@gaptronic.com>
2019-06-04 18:55:06 -0700

    added screen reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -15,6 +15,7 @@ import { reducer as formReducer } from 'redux-form'
 // import all reducers here
 import userReducer from './userReducer';
 import assessmentReducer from "./assessmentReducer";
+import screenReducer from "./screenReducer";
 
 
 // combine reducers
@@ -22,6 +23,7 @@ const reducers = combineReducers({
     form: formReducer,
     user: userReducer,
     assessment: assessmentReducer,
+    screens: screenReducer
 });
 
 // make the combined reducers available for import
