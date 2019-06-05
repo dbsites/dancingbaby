47287b218a4236dc1a6ee8e4a71f81b46bfdc713
neilkatz <nkatz@gaptronic.com>
2019-06-04 18:53:29 -0700

    any static string value

---
+++ client/constants/strings.js
@@ -0,0 +1,18 @@
+/**
+ * ************************************
+ *
+ * @module  strings
+ * @author katzman
+ * @date    06/04/2019
+ * @description
+ *
+ * ************************************
+ */
+
+
+export const SCREEN_LOGIN = 'SCREEN_LOGIN';
+export const SCREEN_DISCLAIMER = 'SCREEN_DISCLAIMER';
+export const SCREEN_ASSESSMENT_GETINFO = 'SCREEN_ASSESSMENT_GETINFO';
+export const SCREEN_ASSESSMENT_QUESTIONS = 'SCREEN_ASSESSMENT_QUESTIONS';
+export const SCREEN_ASSESSMENT_CALCULATING = 'SCREEN_ASSESSMENT_CALCULATING';
+export const SCREEN_ASSESSMENT_RESULTS = 'SCREEN_ASSESSMENT_RESULTS';
