085220fe1d78891b90c4b71945bf264a678ad4ae
neilkatz <nkatz@gaptronic.com>
2019-06-13 20:39:21 -0700

    updated for CSS

--- client/components/AssessmentInfoComponent.jsx
+++ client/components/AssessmentInfoComponent.jsx
@@ -1,7 +1,7 @@
 /**
  * ************************************
  *
- * @module  LoginComponent
+ * @module  AssessmentInfoComponent
  * @author  katzman
  * @date    05/22/2019
  * @description
@@ -28,42 +28,44 @@ class ContentBox extends React.Component
         const { url_id, title_id, fileType_id, title } = this.props;
 
         return (
-            <div>
-                <div>{title}</div>
-                <div className='urlField'>
-                    <label htmlFor={url_id}>URL</label>
-                    <Field
-                        className='inputField'
-                        name={url_id}
-                        component='input'
-                        type='text'
-                        placeholder="Enter Video Link"
-                    />
-                </div>
+            <div className='contentBox'>
+                <div className='title'>{title}</div>
+                <div className='inputFields'>
+                    <div className='urlField'>
+                        <label htmlFor={url_id}>URL:</label>
+                        <Field
+                            className='inputField'
+                            name={url_id}
+                            component='input'
+                            type='text'
+                            placeholder="Enter Video Link"
+                        />
+                    </div>
 
-                <div>OR</div>
+                    <div className='or'>OR</div>
 
-                <div className='contentTitleField'>
-                    <label htmlFor={title_id}>Content Title</label>
-                    <Field
-                        className='inputField'
-                        name={title_id}
-                        component='input'
-                        type='text'
-                        placeholder="Content Title"
-                    />
-                </div>
+                    <div className='contentTitleField'>
+                        <label htmlFor={title_id}>Content Title:</label>
+                        <Field
+                            className='inputField'
+                            name={title_id}
+                            component='input'
+                            type='text'
+                            placeholder="Content Title"
+                        />
+                    </div>
 
-                <div className='fileTypeField'>
-                    <label htmlFor={fileType_id}>File Type</label>
-                    <Field
-                        name={fileType_id}
-                        component="select">
-                        <option value="">Select File Type</option>
-                        <option value="ff0000">Red</option>
-                        <option value="00ff00">Green</option>
-                        <option value="0000ff">Blue</option>
-                    </Field>
+                    <div className='fileTypeField'>
+                        <label htmlFor={fileType_id}>File Type:</label>
+                        <Field
+                            name={fileType_id}
+                            component="select">
+                            <option value="">Select File Type</option>
+                            <option value="ff0000">Red</option>
+                            <option value="00ff00">Green</option>
+                            <option value="0000ff">Blue</option>
+                        </Field>
+                    </div>
                 </div>
             </div>
         )
@@ -76,17 +78,17 @@ let AssessmentInfoComponent = ( props ) =>
     const { handleSubmit, onSubmit } = props;
 
     return (
-        <div >
+        <div className='assessmentInfoComponent'>
 
             <div className='logoContainer'>
                 <img src={dbLogo} className='logo' alt='logo' />
             </div>
 
-            <div>Copyright Fair Use Algorithm</div>
+            <div className='subHead'>Copyright Fair Use Algorithm</div>
 
-            <form className='loginForm' onSubmit={handleSubmit( onSubmit )}>
+            <form className='assessmentForm' onSubmit={handleSubmit( onSubmit )}>
 
-                <div className='fieldieldLeft'>
+                <div className='firstName'>
                     <label htmlFor={strings.ASSESSMENT_INFO_IDS.FIRST_NAME}>FIRST NAME</label>
                     <Field
                         className='inputField'
@@ -97,7 +99,7 @@ let AssessmentInfoComponent = ( props ) =>
                     />
                 </div>
 
-                <div className='fieldMiddle'>
+                <div className='lastName'>
                     <label htmlFor={strings.ASSESSMENT_INFO_IDS.LAST_NAME}>LAST NAME</label>
                     <Field
                         className='inputField'
@@ -108,7 +110,7 @@ let AssessmentInfoComponent = ( props ) =>
                     />
                 </div>
 
-                <div className='fieldRight'>
+                <div className='orgName'>
                     <label htmlFor={strings.ASSESSMENT_INFO_IDS.ORG_NAME}>ORGANIZATION NAME</label>
                     <Field
                         className='inputField'
@@ -119,7 +121,7 @@ let AssessmentInfoComponent = ( props ) =>
                     />
                 </div>
 
-                <div>
+                <div className='contentBoxes'>
                     <ContentBox
                         url_id={strings.ASSESSMENT_INFO_IDS.URL_COPYRIGHTED}
                         title_id={strings.ASSESSMENT_INFO_IDS.TITLE_COPYRIGHTED}
