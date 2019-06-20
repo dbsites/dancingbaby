25d517c237e6fac88c058a9841513e57bd615548
neilkatz <nkatz@gaptronic.com>
2019-06-19 23:17:24 -0700

    tracking progress, use values, current question

--- client/reducers/assessmentReducer.js
+++ client/reducers/assessmentReducer.js
@@ -15,11 +15,20 @@ import * as strings from '../constants/strings';
 
 const initialState = {
 
+    progress: 0,
+
     // questions
     questions: [],
     currentQuestion:{},
     currentQuestionIndex: 0,
     totalQuestions: 0,
+    questionsComplete:false,
+
+    // fair use
+    noFairUse: 0,
+    noInfringement: 0,
+    yesFairUse: 0,
+    yesInfringement: 0,
 
     // assessment info
     [strings.ASSESSMENT_INFO_IDS.FIRST_NAME]:'',
@@ -35,22 +44,24 @@ const initialState = {
     [strings.ASSESSMENT_INFO_IDS.URL_SUSPECTED]:'',
     [strings.ASSESSMENT_INFO_IDS.TITLE_SUSPECTED]:'',
     [strings.ASSESSMENT_INFO_IDS.FILETYPE_SUSPECTED]:'',
-
 };
 
 
 const assessmentReducer = ( state=initialState, action ) =>
 {
 
+    let questions;
+
     switch( action.type )
     {
 
         // going to grab questions from successful login here and add them to state
         case types.USER_LOGIN_SUCCESS:
-            const questions = getQuestions( action.payload );
+            questions = getQuestions( action.payload );
             return {
                 ...state,
-                questions
+                questions,
+                totalQuestions:questions.length
             };
 
         case types.ASSESSMENT_INFO_SUBMIT:
@@ -76,9 +87,23 @@ const assessmentReducer = ( state=initialState, action ) =>
             };
 
         // going to update question count, current fairuse value, update progress bar.
-        case types.QUESTION_ANSWERED:
+        case types.ASSESSMENT_UPDATE:
+            // console.log( "ASSESSMENT_UPDATE: ", state.questions[action.payload.index], action.payload.response );
+
+            const currentQuestionIndex = action.payload.index+1;
+            const currentQuestion = state.questions[action.payload.index];
+
+            currentQuestion.isAnswered = action.payload.response;
+
+            questions = getSubquestions( state.questions, currentQuestion, action.payload );
+            const fairUseValues = getFairuseValues( state, currentQuestion, action.payload.response );
+
             return {
-                ...state
+                ...state,
+                ...fairUseValues,
+                questions,
+                currentQuestionIndex,
+                progress:action.payload.index/(state.questions.length-1),
             };
 
         default:
@@ -89,22 +114,47 @@ const assessmentReducer = ( state=initialState, action ) =>
 export default assessmentReducer;
 
 
-const question = ( value ) =>
+const getSubquestions = ( questions, question, payload ) =>
+{
+    if( !question.branchOn || question.branchOn.toLowerCase() !== payload.response.toLowerCase() || !question.subQuestions ) return questions;
+
+    const subQuestions = getQuestions( question.subQuestions, true );
+    const updateQuestions = [].concat( questions );
+
+    updateQuestions.splice( payload.index+1, 0, ...subQuestions );
+
+    return updateQuestions;
+};
+
+const getFairuseValues = ( state, question, response ) =>
+{
+    if( response === 'unsure' ) return null;
+
+    return {
+        [`${response}FairUse`]:state[`${response}FairUse`] + parseFloat(question[`${response}FairUse`]),
+        [`${response}Infringement`]:state[`${response}Infringement`] + parseFloat(question[`${response}Infringement`])
+    }
+};
+
+const question = ( value, isSubQuestion = false ) =>
 {
     return {
-        question:'yes',
+        isAnswered:false,
+        isSubQuestion,
+
         branchOn: value.branchOn || '',
-        noFairUse: value.noFairUse,
-        noInfringement: value.noInfringement,
         questionNumber: value.questionNumber,
         questionText: value.questionText,
         subQuestions: getQuestions(value.subQuestions),
+
+        noFairUse: value.noFairUse,
+        noInfringement: value.noInfringement,
         yesFairUse: value.yesFairUse,
         yesInfringement: value.yesInfringement
     }
 };
 
-const getQuestions = ( list ) =>
+const getQuestions = ( list, isSubQuestion ) =>
 {
     const questions = [];
 
@@ -112,7 +162,7 @@ const getQuestions = ( list ) =>
     {
         list.forEach(( item ) =>
         {
-            questions.push( new question( item ));
+            questions.push( new question( item, isSubQuestion ));
         })
     }
 
