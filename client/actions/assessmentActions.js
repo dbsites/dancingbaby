b39ffe688b37eb145343fdf13ac4e3c5de8f8db9
neilkatz <nkatz@gaptronic.com>
2019-06-23 21:18:01 -0700

    finally got Youtube video info to fucking WORK!

--- client/actions/assessmentActions.js
+++ client/actions/assessmentActions.js
@@ -11,7 +11,9 @@
 
 import * as types from '../constants/actionTypes';
 import * as screenActions from './screenActions';
+import * as strings from '../constants/strings';
 import Services from '../services/services';
+
 
 
 const submitAssessmentSuccess = ( result ) =>
@@ -56,9 +58,31 @@ export const updateAssessment = ( question ) =>
 
 export const setAssessmentInfo = ( info ) => ( dispatch ) =>
 {
-    console.log( "SET ASSESSMENT INFO" );
-    dispatch( submitAssessmentInfo( info ));
-    dispatch( screenActions.nextScreen() );
+    const suspectId = getYTVideoId( info[strings.ASSESSMENT_INFO_IDS.URL_SUSPECTED] );
+    const copyrightId = getYTVideoId( info[strings.ASSESSMENT_INFO_IDS.URL_COPYRIGHTED] );
+
+
+    const copyrightVideoInfo = new Promise(( resolve, reject ) =>
+    {
+        getYoutubeVideoInfo( copyrightId, strings.ASSESSMENT_INFO_IDS.URL_COPYRIGHTED, resolve );
+    });
+
+    const suspectVideoInfo = new Promise(( resolve, reject ) =>
+    {
+        getYoutubeVideoInfo( suspectId, strings.ASSESSMENT_INFO_IDS.URL_SUSPECTED, resolve );
+    });
+
+    Promise.all( [copyrightVideoInfo, suspectVideoInfo] ).then(( result ) =>
+    {
+        console.log( "SET ASSESSMENT INFO: ", copyrightId, suspectId, result );
+
+        // this will show the results screen.
+        dispatch( screenActions.nextScreen() );
+        dispatch( submitAssessmentInfo({ info, youtubeVideo:result }));
+    });
+
+    // this will show the loading screen.
+    // dispatch( screenActions.nextScreen() );
 };
 
 
@@ -75,6 +99,44 @@ export const submitCompletedAssessment = ( data ) => dispatch =>
         {
             console.log( "ON ERROR IN ACTION: ", res );
             dispatch(submitAssessmentError( res ))
+        }
+    );
+};
+
+
+const getYTVideoId = ( path ) =>
+{
+    let id = '';
+
+    const url = path.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+
+    if( url[2] !== undefined )
+    {
+        id = url[2].split(/[^0-9a-z_\-]/i);
+        id = id[0];
+    }
+    else
+    {
+        id = null;
+    }
+
+    return id;
+};
+
+
+const getYoutubeVideoInfo = ( videoId, videoType, resolve ) =>
+{
+
+    Services.getYoutubeVideoInfo( videoId,
+        ( res ) => // on success
+        {
+            console.log( "ON SUCCESS IN YOUTUBE ACTION: ", res.title, videoType );
+            resolve({ res, type:videoType });
+        },
+        ( error ) => // on error or unauthorized
+        {
+            console.log( "ON ERROR IN ACTION: ", error );
+            resolve({ error, type:videoType });
         }
     );
 };
