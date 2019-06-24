2a6f067e6e89bc133cff7b753febdd08d77582cf
neilkatz <nkatz@gaptronic.com>
2019-06-23 21:19:10 -0700

    updates for youtube video info

--- client/services/services.js
+++ client/services/services.js
@@ -13,8 +13,11 @@
 const routes = {
     userLoginRoute: '/api/login',
     assessmentRoute: '/api/assessment',
+    youtubeRoute: 'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id={videoId},-VoFbH8jTzE&key={apiKey}',
+    youtubeNonAPIRoute: 'https://noembed.com/embed?url=https://www.youtube.com/watch?v={videoId}'
 };
 
+const YOUTUBE_API_KEY = 'AIzaSyDapyM9CSUYKshB1MnQQ2mv4deUwRjvW7k';
 
 const Services = {};
 
@@ -88,5 +91,41 @@ Services.assessmentSubmitRoute = ( assessmentData, onSuccess, onError ) =>
             }
         );
 };
+
+Services.getYoutubeVideoInfo = ( videoid, onSuccess, onError ) =>
+{
+    // const data = JSON.stringify( assessmentData );
+    // const youTubeRoute = routes.youtubeRoute.split('{videoId}').join('3Wf29RiKp70').split('{apiKey}').join(YOUTUBE_API_KEY);
+    const youTubeRoute = routes.youtubeNonAPIRoute.split('{videoId}').join(videoid);
+
+    fetch(youTubeRoute,
+        {
+            method: 'POST',
+            // body: data,
+            headers: {'Content-Type': 'application/json'},
+        })
+        .then( res =>
+        {
+            if( res.ok )
+            {
+                return res.json();
+            }
+
+            if( onError ) onError( res );
+        })
+        .then( res =>
+            {
+                console.log( "YOU TUBE RES: ", res );
+
+                if( onSuccess ) onSuccess( res );
+            }
+        )
+        .catch( err =>
+            {
+                if( onError ) onError( err )
+            }
+        );
+};
+
 
 export default Services
