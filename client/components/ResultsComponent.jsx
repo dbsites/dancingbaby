1bcb7ce636c9d278f75229f5e80873063e0f2626
neilkatz <nkatz@gaptronic.com>
2019-06-30 19:23:15 -0700

    results gauge now works, animates to proper position, the world is good

--- client/components/ResultsComponent.jsx
+++ client/components/ResultsComponent.jsx
@@ -11,65 +11,88 @@
 
 
 import React from 'react';
+import { TweenLite, Back, Circ, Sine } from 'gsap';
 import dbLogo from '../assets/svg/db_logo_greenyellow.svg';
-import resultGage from '../assets/svg/arrowAndGradient.svg';
+import Utils from "../js/Utils";
 
 
-const ResultsComponent = ( props ) =>
+export default class ResultsComponent extends React.Component
 {
 
-    const { titleSuspect, resultText, fairUse, infringement, resultInfringement, startOver, downloadReport } = props;
+    componentDidMount()
+    {
+        this.animateGauge();
+        this.setLegendElementBold();
+    }
 
-    const resultTitle = `${titleSuspect} exhibits a:`;
-    const resultTextIndicator = <span><span className={resultText.color}>{resultText.txt}</span> indication of fair use.</span>;
+    animateGauge()
+    {
+        const rotateTo = 180 * ( 1 - this.props.resultInfringement );
+        TweenLite.to( '.resultGageArrowSVG', 3, {rotation:rotateTo, transformOrigin:"50% 50%", ease:Sine.easeInOut});
+    }
 
-    const factorsAgainst = `Number of factors AGAINST FAIR USE:${infringement}`;
-    const factorsTowards = `Number of factors pointing towards FAIR USE:${fairUse}`;
+    setLegendElementBold()
+    {
+        if( !this.props.resultText ) return;
 
-    return (
-        <div className="resultsComponent">
+        const element = document.querySelector( `.${this.props.resultText.legendClass}` );
+        Utils.addClass( element, 'currentLegendElement' );
+    }
 
-            <div className='logoUpperRighContainer'>
-                <img src={dbLogo} className='logoSmallUpperRight' alt='logo' />
-            </div>
+    render()
+    {
+        const { titleSuspect, resultText, fairUse, infringement, resultInfringement, startOver, downloadReport } = this.props;
 
-            <div className='titleContainer'>RESULTS</div>
+        const resultTitle = `${titleSuspect} exhibits a:`;
+        const resultTextIndicator = resultText ? <span><span className={resultText.color}>{resultText.txt}</span> indication of fair use.</span> : 'indication of fair use.';
 
-            <div className='subTitleContainer'>Your input has been analyzed using the Dancing Baby™ Algorithm.</div>
+        const factorsAgainst = `Number of factors AGAINST FAIR USE:${infringement}`;
+        const factorsTowards = `Number of factors pointing towards FAIR USE:${fairUse}`;
 
-            <div className='resultsContent' >
-                <div className='resultTitle' >{resultTitle}</div>
-                <div className='resultGage' >
-                    <img src={resultGage} className='resultGageSVG' alt='logo' />
+        return (
+
+            <div className="resultsComponent">
+
+                <div className='logoUpperRighContainer'>
+                    <img src={dbLogo} className='logoSmallUpperRight' alt='logo' />
                 </div>
-                <div className='resultColorLegend' >
-                    <div className='veryStrong'>VERY STRONG Indication of Fair Use</div>
-                    <div className='strong'>STRONG Indication of Fair Use</div>
-                    <div className='moderate'>MODERATE Indication of Fair Use</div>
-                    <div className='weak'>WEAK Indication of Fair Use</div>
-                    <div className='noEvidence'>No Evidence of Fair Use</div>
-                </div>
-                <div className='textIndicatorContainer'>
-                    <div className='textIndicator'>
-                        { resultTextIndicator }
+
+                <div className='titleContainer'>RESULTS</div>
+
+                <div className='subTitleContainer'>Your input has been analyzed using the Dancing Baby™ Algorithm.</div>
+
+                <div className='resultsContent' >
+                    <div className='resultTitle' >{resultTitle}</div>
+                    <div className='resultGage' >
+                        <div className='resultGageSVG' />
+                        <div className='resultGageArrowSVG' />
+                    </div>
+                    <div className='resultColorLegend' >
+                        <div className='veryStrong'>VERY STRONG Indication of Fair Use</div>
+                        <div className='strong'>STRONG Indication of Fair Use</div>
+                        <div className='moderate'>MODERATE Indication of Fair Use</div>
+                        <div className='weak'>WEAK Indication of Fair Use</div>
+                        <div className='noEvidence'>No Evidence of Fair Use</div>
+                    </div>
+                    <div className='textIndicatorContainer'>
+                        <div className='textIndicator'>
+                            { resultTextIndicator }
+                        </div>
+                    </div>
+                    <div className='factorsContainer'>
+                        <div className='factorsAgainst'>
+                            { factorsAgainst }
+                        </div>
+                        <div className='factorsTowards'>
+                            { factorsTowards }
+                        </div>
                     </div>
                 </div>
-                <div className='factorsContainer'>
-                    <div className='factorsAgainst'>
-                        { factorsAgainst }
-                    </div>
-                    <div className='factorsTowards'>
-                        { factorsTowards }
-                    </div>
+
+                <div className='downloadBtnContainer'>
+                    <button className='downloadBtn' onClick={downloadReport} type='submit'>DOWNLOAD REPORT</button>
                 </div>
             </div>
-
-            <div className='downloadBtnContainer'>
-                <button className='downloadBtn' onClick={downloadReport} type='submit'>DOWNLOAD REPORT</button>
-            </div>
-        </div>
-    );
-};
-
-
-export default ResultsComponent;
+        );
+    }
+}
