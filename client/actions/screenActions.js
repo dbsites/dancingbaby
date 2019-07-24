964ca4b6251a809ef75869d9938bcc4405283c0d
neilkatz <nkatz@gaptronic.com>
2019-07-23 21:03:05 -0700

    moved start over to screenActions

--- client/actions/screenActions.js
+++ client/actions/screenActions.js
@@ -18,3 +18,11 @@ export const nextScreen = () =>
     type: types.NEXT_SCREEN,
     payload: null
 });
+
+
+
+export const startOver = () =>
+({
+    type: types.ASSESSMENT_START_OVER,
+    payload: null
+});
