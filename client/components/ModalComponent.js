829077733d4b7f685b9b6d67ec8b527f18c69878
neilkatz <nkatz@gaptronic.com>
2019-07-28 14:05:07 -0700

    modal component

---
+++ client/components/ModalComponent.js
@@ -0,0 +1,53 @@
+/**
+ * Created by neilkatz on 3/22/17.
+ */
+
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+
+
+const modalTypes = {
+    terms:{
+        title:'TERMS OF USE',
+        body:'Terms of use text'
+    },
+    privacy:{
+        title:'PRIVACY POLICY',
+        body:'Privacy policy text'
+    }
+};
+
+
+export default class ModalComponent extends React.Component
+{
+
+    render()
+    {
+
+        const { showHideModal, modalData } = this.props;
+
+        const title = modalData ? modalTypes[modalData].title : "";
+        const body = modalData ? modalTypes[modalData].body : "";
+        const show = !!modalData;
+
+        console.log( "SHOW MODAL: ", show, modalData );
+
+        return (
+            <Modal show={show} onHide={ showHideModal.bind( null, false )} bsSize="large" centered >
+
+                <Modal.Header closeButton>
+                    <Modal.Title>{title}</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    {body}
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <button onClick={ showHideModal.bind( null, false )} >CLOSE</button>
+                </Modal.Footer>
+
+            </Modal>
+        )
+    }
+};
